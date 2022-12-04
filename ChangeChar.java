d6e5619857e2f15f1eba3a61c2e53b73d6748d79
PC-202211211726\Administrator <michaelpluto2000@outlook.com>
2022-12-04 17:27:58 +0800

    the first day

---
+++ ChangeChar.java
@@ -0,0 +1,11 @@
+public class ChangeChar{
+	public static void main(String[] args) {
+		System.out.println("北京\t天津\t上海\t ");
+		System.out.println("jack\nsmith\nmary");
+		System.out.println("C:\\Windows\\System32\\cmd.exe");	
+		System.out.println("C:\\\\Windows\\\\System32\\\\cmd.exe");
+		System.out.println("我说:\"要好好学习java有前途\"");
+		System.out.println("我说:\'为了去日本工作\'");
+
+	}
+}
---
+++ Test1.java
@@ -0,0 +1,15 @@
+public class Test1{
+	public static void main(String[] args) {
+		System.out.println("杨质飞 in studying ");
+	}
+}
+class Dog{
+	public static void main(String[] args){
+		System.out.println("hello,小狗");
+	}
+}
+class Tiger{
+	public static void main(String[] args){
+		System.out.println("hello,老虎先生");
+	}
+}
