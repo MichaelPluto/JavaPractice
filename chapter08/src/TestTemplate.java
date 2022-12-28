e2460b7b7fe0d3d1cb7ac96bbcad8549109b2d9c
PC-202211211726\Administrator <michaelpluto2000@outlook.com>
2022-12-28 18:36:45 +0800

    12.28

---
+++ chapter08/src/TestTemplate.java
@@ -0,0 +1,16 @@
+public class TestTemplate {
+    //main 模板快捷键
+    public static void main(String[] args) {
+
+        //sout 模板快捷键
+        System.out.println();
+        //fori for 循环快捷键
+        for (int i = 0; i < 2; i++) {
+
+        }
+        //自创的快捷键
+        int arr [] = {1, 3, 4, 5};
+        for(int i = 0; i < arr.length; i++){
+        }
+    }
+}
