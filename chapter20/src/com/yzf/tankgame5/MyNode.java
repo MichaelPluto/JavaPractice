3eea506a788ca227da7d7527f59e587675d01c9f
PC-202211211726\Administrator <michaelpluto2000@outlook.com>
2023-02-28 20:29:36 +0800

    2.28

---
+++ chapter20/src/com/yzf/tankgame5/MyNode.java
@@ -0,0 +1,37 @@
+package com.yzf.tankgame5;
+
+public class MyNode {
+    private int x;
+    private int y;
+    private int direction;
+
+    public MyNode(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+}
---
+++ chapter20/src/com/yzf/tankgame5/TankGame05.java
@@ -0,0 +1,39 @@
+package com.yzf.tankgame5;
+
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Scanner;
+
+public class TankGame05 extends JFrame{
+    //定义MyPanel
+    MyPanel mp = null;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        TankGame05 tankGame01 = new TankGame05();
+    }
+
+    public TankGame05() {
+        System.out.println("请输入选择 1: 新游戏 2: 继续游戏");
+        String key = scanner.next();
+        mp = new MyPanel(key);
+        Thread thread = new Thread(mp);
+        thread.start();// 启动线程
+        this.add(mp);//面板就是游戏的绘图区域
+        this.setSize(1300,950);//窗口大小
+        this.addKeyListener(mp);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        //在JFrame中 增加相应关 闭窗口的处理
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Recorder.saveAllEnemyTankNum();
+
+                System.exit(0);
+            }
+        });
+    }
+}
